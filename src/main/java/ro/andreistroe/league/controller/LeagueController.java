7f6020673fc990ac09edf65ccfc7d7d6faf24022
Andrei-Cătălin Stroe <acstroe@axway.com>
2019-03-05 13:12:02 +0200

    display initial points

--- src/main/java/ro/andreistroe/league/controller/LeagueController.java
+++ src/main/java/ro/andreistroe/league/controller/LeagueController.java
@@ -3,7 +3,6 @@ package ro.andreistroe.league.controller;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
